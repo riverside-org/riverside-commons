e0900bdb8ecb6d3eb4d90d3f9e26b77d11d0b2f0
Javid Singh <javidjs@uci.edu>
2022-06-18 04:38:44 -0700

    Extend document object

--- src/main/java/com/riverside/commons/model/dao/gateway/ServiceResponse.java
+++ src/main/java/com/riverside/commons/model/dao/gateway/ServiceResponse.java
@@ -1,14 +1,26 @@
 package com.riverside.commons.model.dao.gateway;
 
+import com.riverside.commons.model.dao.DocumentObject;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 @Data
-public class ServiceResponse {
+public class ServiceResponse extends DocumentObject {
     @Id
     private String transactionId;
     private String email;
     private String sessionId;
     private String response;
     private int httpStatus;
+
+    @Builder
+    public ServiceResponse(String userId, String email, String sessionId, String response, int httpStatus) {
+        super(userId);
+
+        this.email = email;
+        this.sessionId = sessionId;
+        this.response = response;
+        this.httpStatus = httpStatus;
+    }
 }
